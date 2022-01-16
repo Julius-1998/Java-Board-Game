1a8388bd377451eb915e8e65686329b18605eb1b
Julius-1998 <sz232@duke.edu>
2022-01-16 13:00:45 -0800

    task8

--- src/test/java/edu/duke/sz232/battleship/BoardTextViewTest.java
+++ src/test/java/edu/duke/sz232/battleship/BoardTextViewTest.java
@@ -2,6 +2,10 @@ package edu.duke.sz232.battleship;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.LinkedList;
+
+import javax.swing.plaf.SliderUI;
+
 import org.junit.jupiter.api.Test;
 
 public class BoardTextViewTest {
@@ -37,6 +41,38 @@ public class BoardTextViewTest {
                 "E  | |  E\n";
         emptyBoardHelper(3, 5, expectedHeader, expectedBody);
     }
+
+    @Test
+    public void test_dispaly_single_basic_ship_3by5(){
+        LinkedList<Ship<Character>> shipList = new LinkedList<>();
+        BasicShip b1 =  new BasicShip(new Coordinate(0, 1));
+        shipList.add(b1);
+        String expectedBody = 
+                "A  |s|  A\n" +
+                "B  | |  B\n" +
+                "C  | |  C\n" +
+                "D  | |  D\n" +
+                "E  | |  E\n";
+        notEmptyBoardHelper(3, 5, expectedBody,shipList);
+    }
+
+    @Test
+    public void test_dispaly_multiple_basic_ship_3by5(){
+        LinkedList<Ship<Character>> shipList = new LinkedList<>();
+        BasicShip b1 =  new BasicShip(new Coordinate(0, 1));
+        BasicShip b2 =  new BasicShip(new Coordinate(0, 0));
+        BasicShip b3 =  new BasicShip(new Coordinate(4, 2));
+        shipList.add(b1);
+        shipList.add(b2);
+        shipList.add(b3);
+        String expectedBody = 
+                "A s|s|  A\n" +
+                "B  | |  B\n" +
+                "C  | |  C\n" +
+                "D  | |  D\n" +
+                "E  | |s E\n";
+        notEmptyBoardHelper(3, 5, expectedBody,shipList);
+    }
     /**
      * Helper function for generate board test
      * @param w width of the board
@@ -48,6 +84,17 @@ public class BoardTextViewTest {
         Board<Character> b1 = new BattleShipBoard<Character>(w, h);
         BoardTextView view = new BoardTextView(b1);
         assertEquals(expectedHeader, view.makeHeader());
+        String expected = expectedHeader + expectedBody + expectedHeader;
+        assertEquals(expected, view.displayMyOwnBoard());
+    }
+    
+    private void notEmptyBoardHelper(int w, int h, String expectedBody,LinkedList<Ship<Character>> list){
+        Board<Character> b1 = new BattleShipBoard<Character>(w, h);
+        for (Ship<Character> ship : list) {
+            b1.tryAddShip(ship); 
+        }
+        BoardTextView view = new BoardTextView(b1);
+        String expectedHeader = view.makeHeader();
         String expected = expectedHeader + expectedBody + expectedHeader;
         assertEquals(expected, view.displayMyOwnBoard());
     }
